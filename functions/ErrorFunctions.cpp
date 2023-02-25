20f59c8689fc8679d27097afd0f84663b6c5487f
mateasmario <mmmateas@gmail.com>
2023-02-25 11:26:31 +0200

    Fixed error not mentioning additional details after code reorganization

--- functions/ErrorFunctions.cpp
+++ functions/ErrorFunctions.cpp
@@ -3,14 +3,14 @@
 #include <stdlib.h>
 
 void err(const char* fmt) {
-	fprintf(stderr, "Error: ");
+	fprintf(stderr, "Error: %s", fmt);
 	fputc('\n', stderr);
 	exit(-1);
 }
 
 void lineErr(const char* fmt, int line)
 {
-	fprintf(stderr, "Error on line %d: ", line);
+	fprintf(stderr, "Error on line %d: %s", line, fmt);
 	fputc('\n', stderr);
 	exit(-1);
 }
